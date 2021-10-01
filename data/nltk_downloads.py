6893854f2addb044cbeedfd3f2f94ed1dad4c77b
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-10-01 16:10:23 +0100

    verbalize nltk downloads

--- data/nltk_downloads.py
+++ data/nltk_downloads.py
@@ -1,6 +1,6 @@
 import nltk
 
-#nltk.download('vader_lexicon')
-#nltk.download('stopwords')
-#nltk.download('punkt')
-#nltk.download('popular')
+nltk.download('vader_lexicon')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('popular')
